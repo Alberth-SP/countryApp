c7823769f0017c14e9f0fc9441b570f196b6026c
Alberthsp <alberth.cb92@gmail.com>
2024-11-19 22:17:58 -0500

    agregando componente search y result

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -4,6 +4,8 @@ import { ByPaisPageComponent } from './page/by-pais-page/by-pais-page.component'
 import { ByRegionPageComponent } from './page/by-region-page/by-region-page.component';
 import { ByCapitalPageComponent } from './page/by-capital-page/by-capital-page.component';
 import { CountryRoutingModule } from './country-routing.module';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { ResultsTableComponent } from './components/results-table/results-table.component';
 
 
 
@@ -11,7 +13,9 @@ import { CountryRoutingModule } from './country-routing.module';
   declarations: [
     ByPaisPageComponent,
     ByRegionPageComponent,
-    ByCapitalPageComponent
+    ByCapitalPageComponent,
+    SearchBoxComponent,
+    ResultsTableComponent
   ],
   imports: [
     CommonModule,
